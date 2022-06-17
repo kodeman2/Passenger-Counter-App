eaa63d3e88507b50115bd3cd003a2815910c7094
Olagunju Oluwakolade <holuwakoladey@gmail.com>
2022-06-17 02:19:58 +0100

    new

---
+++ index.js
@@ -0,0 +1,11 @@
+document.getElementById("count-el").innerText = 5
+
+let count = 5
+count = count + 1
+console.log(count)
+
+//initialize the count as 0
+//listen for clicks on the increent button
+
+
+
