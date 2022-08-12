c029526286c2fade6ea3d8e0c5b9c69a25b3c97b
Olagunju Oluwakolade <holuwakoladey@gmail.com>
2022-08-12 09:19:24 +0100

    new updates

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
-document.getElementById("count-el").innerText = 5
+// document.getElementById("count-el").innerText = 5
 
-let count = 5
-count = count + 1
-console.log(count)
-
-//initialize the count as 0
-//listen for clicks on the increent button
-
-
-
+ //initialize the count as 0
+//listen for clicks on the increment button
+//increment the count variable when button is clicked
+//change thr count-el in the html to reflect the new count
+let countEl = document.getElementById("count-el")
+console.log(countEl)
+let count = 0
+function increment() {
+ count = count + 1
+ countEl.innerText = count
+ console.log(count)
+}
+function save() {
+ console.log(count)
+}
+save()
