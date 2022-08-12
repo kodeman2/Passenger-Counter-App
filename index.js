88eba6c0003e37bd737d3c59eab04e9aa7c9d0bd
Olagunju Oluwakolade <holuwakoladey@gmail.com>
2022-08-12 11:23:59 +0100

    passanger counter app ready

--- index.js
+++ index.js
@@ -4,15 +4,18 @@
 //listen for clicks on the increment button
 //increment the count variable when button is clicked
 //change thr count-el in the html to reflect the new count
+let saveEl = document.getElementById("save-el")
 let countEl = document.getElementById("count-el")
-console.log(countEl)
+
 let count = 0
 function increment() {
- count = count + 1
- countEl.innerText = count
- console.log(count)
+ count += 1
+ countEl.textContent = count
+ 
 }
 function save() {
- console.log(count)
+ let newp = count + " - " 
+ saveEl.textContent += newp
+ countEl.textContent = 0
+ count = 0 
 }
-save()
